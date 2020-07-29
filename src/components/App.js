0177adba9e8ce79467e0f6bcf387ddfac0769426
Sokos6 <will.sokolowski@gmail.com>
2020-07-28 22:48:42 -0500

    continue project setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Our Journey 1.0
+    </div>
+  );
+}
+
+export default App;
