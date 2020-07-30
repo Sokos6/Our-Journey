b7b7eeb64328b300a278957206903d340ca9d0a4
Sokos6 <will.sokolowski@gmail.com>
2020-07-29 19:38:47 -0500

    app.js update with apollo

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/App.css';
+import Header from './Header.js';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { setContext } from 'apollo-link-context';
 
 function App() {
+  // Apollo Client
+  const httpLink = new HttpLink({
+    uri: 'https://our-journey-backend.herokuapp.com/v1/graphql',
+  });
+
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <div className="App">
-      Our Journey 1.0
-    </div>
+    <ApolloProvider client={client}>
+      <Header />
+    </ApolloProvider>
   );
 }
 
