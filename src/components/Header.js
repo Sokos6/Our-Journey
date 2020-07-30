09f4c593413207ff1966312fed4051cd8e9985b1
Sokos6 <will.sokolowski@gmail.com>
2020-07-29 20:01:46 -0500

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import '../styles/App.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
-  return <div>HEADER</div>;
+  return (
+    <nav className='Nav'>
+      <div className='Nav-menus'>
+        <div className='Nav-brand'>
+          <Link className='Nav-brand-logo' to='/'>
+            Our Journey
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
